af11c9a821b428661618fbd4078d49e6ef8d09c4
Yonghyun Kim <81367886+yonghyun421@users.noreply.github.com>
2024-09-16 18:53:05 +0900

    feat: theme 변경 기능 추가

---
+++ utils/themeEffect.ts
@@ -0,0 +1,11 @@
+export const themeEffect = () => {
+  if (
+    localStorage.theme === "dark" ||
+    (!("theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+};
